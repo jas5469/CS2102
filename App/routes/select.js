6e7cb47394930fc2449308b75dac897e9c905e4f
Jason Leo <jas_p90@hotmail.com>
2019-10-13 22:20:24 +0800

    updated student  info into users

--- App/routes/select.js
+++ App/routes/select.js
@@ -6,12 +6,12 @@ const pool = new Pool({
   user: 'jasonleo',
   host: '/tmp',
   database: 'postgres',
-  \\password: '********',
+  password: 'password',
   port: 5432,
 })
 
 /* SQL Query */
-var sql_query = 'SELECT * FROM student_info';
+var sql_query = 'SELECT * FROM users';
 
 router.get('/', function(req, res, next) {
 	pool.query(sql_query, (err, data) => {

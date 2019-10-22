0291129757d3ad990fca2f2c08d2455b51249d5c
Jason Leo <jas_p90@hotmail.com>
2019-10-22 18:18:44 +0800

    changed to adi i have kept the files last time too

--- App/app.js
+++ App/app.js
@@ -1,40 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const sql_query = require('./sql');
 
-/* --- V7: Using dotenv     --- */
-require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
 
-/* --- V2: Adding Web Pages --- */
-var aboutRouter = require('./routes/about');
-/* ---------------------------- */
+const app = express();
 
-/* --- V3: Basic Template   --- */
-var tableRouter = require('./routes/table');
-var loopsRouter = require('./routes/loops');
-/* ---------------------------- */
+// Body Parser Config
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-/* --- V4: Database Connect --- */
-var selectRouter = require('./routes/select');
-/* ---------------------------- */
+// Authentication Setup
+//require('dotenv').load();
+require('./auth').init(app);
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-/* --- V5: Adding Forms     --- */
-var formsRouter = require('./routes/forms');
-/* ---------------------------- */
 
-/* --- V6: Modify Database  --- */
-var insertRouter = require('./routes/insert');
-var insertProjectRouter = require('./routes/insertproject');
-/* ---------------------------- */
-
-var app = express();
-
-// view engine setup
+// View Engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -44,33 +40,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-/* --- V2: Adding Web Pages --- */
-app.use('/about', aboutRouter);
-/* ---------------------------- */
-
-/* --- V3: Basic Template   --- */
-app.use('/table', tableRouter);
-app.use('/loops', loopsRouter);
-/* ---------------------------- */
-
-/* --- V4: Database Connect --- */
-app.use('/select', selectRouter);
-/* ---------------------------- */
-
-/* --- V5: Adding Forms     --- */
-app.use('/forms', formsRouter);
-/* ---------------------------- */
-
-/* --- V6: Modify Database  --- */
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/insert', insertRouter);
-app.use('/insertproject', insertProjectRouter);
-/* ---------------------------- */
+// Router Setup
+require('./routes').init(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
